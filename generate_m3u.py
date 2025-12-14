b4f42fae7fbfa66cc3f0ae0ad40652fdfbaa686c
gmtv4 <guilhermemartinstv4@gmail.com>
2025-12-14 02:17:52 -0300

    Update generate_m3u.py

--- generate_m3u.py
+++ generate_m3u.py
@@ -18,6 +18,7 @@ if not os.path.exists(cookies_path):
 
 # Lista de URLs de canais do YouTube (pode adicionar mais)
 globoplay_urls = [
+    "https://www.youtube.com/@tvsucessobandgo/live",
     "https://www.youtube.com/@sbtbrasilia/live",
     "https://www.venevisionplay.com/player/channel/66cf63debda7d300166c665c/",  # Venevisión Play
     "https://www.youtube.com/@televisatijuanaof/live",                       # Televisa Tijuana
